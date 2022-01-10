9965698e937f62b824345d2052442525a04b13d0
Sakariash <simonsakarias@gmail.com>
2022-01-10 02:24:25 +0100

    Small tweaks in log in file, error if no user is found

--- app/users/login.php
+++ app/users/login.php
@@ -10,7 +10,6 @@ if (isset($_POST['email'], $_POST['password'])) :
     $email = trim($_POST['email']);
     $password = $_POST['password'];
 
-
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) :
         $_SESSION['errors'][] = 'The email adress is not a valid email adress!';
         redirect('/Test.php');
@@ -22,7 +21,14 @@ if (isset($_POST['email'], $_POST['password'])) :
     $statement->execute();
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
+    if (!$user) :
+        $_SESSION['errors'][] = 'The email address or the password is incorrect.';
+        redirect('/Test.php');
+    endif;
+
+
     if (password_verify($password, $user['password'])) :
+
         unset($user['password']);
 
         $_SESSION['user'] = [
