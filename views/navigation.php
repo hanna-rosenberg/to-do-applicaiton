790c3a02dfa2bde64be1f7dec9e3cf8a0f88af53
Sakariash <simonsakarias@gmail.com>
2022-01-10 02:31:04 +0100

    Navbar to be deleted?

--- views/navigation.php
+++ views/navigation.php
@@ -10,13 +10,19 @@
         <li class="nav-item">
             <a href="/about.php" class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/about.php' ? 'active' : ''; ?> ">About</a>
         </li>
+        <li class="nav-item">
+            <a href="/noUser.php" class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/noUser.php' ? 'active' : ''; ?> ">No user</a>
+        </li>
+        <li class="nav-item">
+            <a href="/AddUser.php" class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/AddUser.php' ? 'active' : ''; ?> ">Include</a>
+        </li>
 
 
         <li class="nav-item">
             <?php if (isset($_SESSION['user'])) : ?>
                 <a href="app/users/logout.php" class="nav-link">Log out</a>
             <?php else : ?>
-                <a href="/login.php" class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>">Log in</a>
+                <a href="/Test.php" class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/Test.php' ? 'active' : ''; ?>">Log in</a>
             <?php endif; ?>
         </li>
 
@@ -34,46 +40,64 @@
             <?php endif; ?>
 
     </ul>
-</nav> -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+</nav>
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
     <a class="navbar-brand" href="#">LOGO</a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <?php if (isset($_SESSION['user'])) : ?>
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="profile-img-nav" src="<?php echo $_SESSION['user']['image']; ?>" alt="profile picture">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+            <!-- <?php if (isset($_SESSION['user'])) : ?> -->
+<!-- <img class="profile-img-nav" src="<?php echo $_SESSION['user']['image']; ?>" alt="profile picture">
+</a>
+<?php else : ?>
+    <span class="navbar-toggler-icon"></span>
+<? endif; ?>
+</button>
+
+
+
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+    <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item active">
+
+            <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
+
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/Test.php">Log in test</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/register.php">Register</a>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
+<!-- <?php if (isset($_SESSION['user'])) : ?> -->
+<!-- <img class="profile-img-nav" src="<?php echo $_SESSION['user']['image']; ?>" alt="profile picture">
+            <? else : ?>
+                <span class="navbar-toggler-icon"></span>
+            <? endif; ?>
             </a>
-        <?php endif; ?>
-        <!-- <span class="navbar-toggler-icon"></span> -->
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/Test.php">Log in test</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/register.php">Register</a>
-            </li>
-            <li class="nav-item dropdown">
-                <?php if (isset($_SESSION['user'])) : ?>
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="profile-img-nav" src="<?php echo $_SESSION['user']['image']; ?>" alt="profile picture">
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                        <a class="dropdown-item" href="/profile.php">Profile</a>
-                        <a class="dropdown-item" href="/index.php">Home</a>
-                        <a class="dropdown-item" href="/register.php">Register</a>
-                        <a class="dropdown-item" href="/Test.php">Test</a>
-                        <a class="dropdown-item" href="app/users/logout.php">Log out</a>
+                <a class="dropdown-item" href="/profile.php">Profile</a>
+                <a class="dropdown-item" href="/index.php">Home</a>
+                <a class="dropdown-item" href="/register.php">Register</a>
+                <a class="dropdown-item" href="/Test.php">Test</a>
+                <a class="dropdown-item" href="app/users/logout.php">Log out</a>
 
-            </li>
+        </li>
 
-    </div>
-<?php endif; ?>
+</div>
+
 </li>
 </ul>
 </div>
-</nav>
+</nav>  -->
