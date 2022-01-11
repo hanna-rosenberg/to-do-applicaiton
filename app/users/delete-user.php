069d6c93280ec16442a6b85bb3a1cf375b8f666f
Sakariash <simonsakarias@gmail.com>
2022-01-12 00:34:20 +0100

    Create delete-user.php

---
+++ app/users/delete-user.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../autoload.php';
+
+if (isset($_POST['password'])) :
+    $user_id = $_SESSION['user']['id'];
+
+    $query = 'SELECT password from users WHERE id = :id';
+
+    $statement = $database->prepare($query);
+    $statement->bindParam(':id', $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if (!password_verify($_POST['password'], $user['password'])) :
+
+        $_SESSION['password-errors'][] = 'You typed in the wrong password, please try again! Your account is still active.';
+
+        redirect('/profile.php');
+
+    else :
+
+        $query = 'DELETE FROM users WHERE id = :id';
+
+        $statement = $database->prepare($query);
+        $statement->bindParam(':id', $user_id, PDO::PARAM_INT);
+        $statement->execute();
+
+        // delete users lists from database
+
+        $query = 'DELETE FROM lists WHERE user_id = :user_id';
+        $statement = $database->prepare($query);
+        $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $statement->execute();
+
+        // delete users tasks from database
+
+        $query = 'DELETE FROM tasks WHERE user_id = :user_id';
+        $statement = $database->prepare($query);
+        $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $statement->execute();
+        unset($_SESSION['user']);
+
+        $_SESSION['confirm'][] = 'The account was successfully deleted.';
+
+        redirect('/Test.php');
+
+    endif;
+
+endif;
