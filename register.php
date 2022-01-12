cc1736ad5eba40be5774fcfc2f72fa7bd2acfee9
Sakariash <simonsakarias@gmail.com>
2022-01-12 18:28:38 +0100

    Small changes in the layout

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
         <?php
         if (isset($_SESSION['errors'])) :
             foreach ($_SESSION['errors'] as $error) : ?>
-                <p class="alert alert-danger"><?= $error ?></p>
+                <p class="alert alert-danger"><?php echo htmlspecialchars($error) ?></p>
         <?php endforeach;
             unset($_SESSION['errors']);
         endif;
@@ -17,7 +17,7 @@
                 <label class="col-md-2 col-sm-3 col-xs-12 control-label">Name</label>
                 <div class="col-md-10 col-sm-9 col-xs-12">
                     <input type="name" class="form-control" name="name" id="name" placeholder="Full name" required>
-                    <small class="form-text">Please provide your full name</small>
+                    <small class="form-text">Please provide your full name.</small>
                 </div>
             </div>
 
@@ -25,24 +25,23 @@
                 <label class="col-md-2 col-sm-3 col-xs-12 control-label">Email</label>
                 <div class="col-md-10 col-sm-9 col-xs-12">
                     <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
-                    <small class="form-text">Please provide your email (passphrase)</small>
+                    <small class="form-text">Please provide your email.</small>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-md-2 col-sm-3 col-xs-12 control-label">Password</label>
                 <div class="col-md-10 col-sm-9 col-xs-12">
                     <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-                    <small class="form-text">Please provide your password (passphrase)</small>
+                    <small class="form-text">Please provide your password.</small>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Repeat password</label>
+                <label class="col-md-5 col-sm-5 col-xs-12 control-label">Repeat password</label>
                 <div class="col-md-10 col-sm-9 col-xs-12">
                     <input type="password" class="form-control" name="password-repeat" id="password" placeholder="Repeat password" required>
-                    <small class="form-text">Please confirm with old password. (passphrase)</small>
+                    <small class="form-text">Please confirm with old password. </small>
                 </div>
             </div>
-
             <hr>
             <div class="form-group">
                 <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
