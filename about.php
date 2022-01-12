e35e0a269fd4db3741885e4e32cfcfbddec38241
Sakariash <simonsakarias@gmail.com>
2022-01-12 18:19:37 +0100

    deleted files

--- about.php
+++
@@ -1,9 +0,0 @@
-<?php require __DIR__ . '/navigation.php'; ?>
-
-
-<article>
-    <h1>About</h1>
-    <p>If you're here something probably went wrong....</p>
-</article>
-
-<?php require __DIR__ . '/views/footer.php'; ?>
